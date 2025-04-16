a64ed2e4401f922fc06bbcc3f82a45e38fbf467a
AbanoubEhab <abanobehab@gmail.com>
2025-04-16 20:58:38 +0200

    adding check update script

---
+++ checkupdate.sh
@@ -0,0 +1,19 @@
+
+lastcommit=$(/usr/bin/curl https://raw.githubusercontent.com/AbanoubEhab/batocera-Arcade/refs/heads/time-stamp/last_commit.txt)
+
+lastupdate=$(/bin/cat /userdata/system/gl-update/last_commit.txt)
+
+if [[ $lastcommit == $lastupdate ]]; then
+
+  echo "No new Updates"
+
+else
+
+  echo "new update found"
+# execute update script
+
+
+echo $lastcommit > /userdata/system/gl-update/last_commit.txt
+
+fi
+
