5c7172500f8536ea316dbcebfd1f0d8c3b3ccbd6
AbanoubEhab <abanobehab@gmail.com>
2025-04-17 14:05:09 +0200

    add splashscreen

--- checkupdate.sh
+++ checkupdate.sh
@@ -12,7 +12,7 @@ else
 
   echo "new update found"
   # execute update script
-
+  curl -fsSL https://github.com/AbanoubEhab/batocera-Arcade/raw/refs/heads/main/update.sh | sh
 
   echo $lastcommit > /userdata/system/gl-update/last_commit.txt
 
---
+++ update.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+wget -P /userdata/splash https://github.com/AbanoubEhab/batocera-Arcade/raw/refs/heads/main/RBstart.mp4
