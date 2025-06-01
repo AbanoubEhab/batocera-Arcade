b759bcab2018913d85e9944b2b6638e0c39e7dea
AbanoubEhab <36665223+AbanoubEhab@users.noreply.github.com>
2025-06-01 13:40:02 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-wget -P /userdata/splash https://github.com/AbanoubEhab/batocera-Arcade/raw/refs/heads/main/RBstart.mp4
+#wget -P /userdata/splash https://github.com/AbanoubEhab/batocera-Arcade/raw/refs/heads/main/RBstart.mp4
